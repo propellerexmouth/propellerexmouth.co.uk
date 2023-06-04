389c9b5120b9fb408f3378713fa370712a4bd001
Alex Howes <alex@pixelfridge.digital>
2023-06-04 16:13:11 +0100

    fix(footer) Add flex shrink to icons and change element for email and phone text

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -20,16 +20,16 @@ const Footer = () => {
         </nav>
         <div id="contact-details" className="space-y-8 ">
           <div id="email" className="flex space-x-2">
-            <EmailIcon />
-            <address className="not-italic">team@propellerexmouth.co.uk</address>
+            <EmailIcon className="flex-shrink-0" />
+            <span className="not-italic">team@propellerexmouth.co.uk</span>
           </div>
           <div id="address" className="flex space-x-2">
-            <PinIcon />
+            <PinIcon className="flex-shrink-0" />
             <address className="not-italic">15 Victoria Road, Exmouth, Devon, EX8 1DL</address>
           </div>
           <div id="phone" className="flex space-x-2">
-            <PhoneIcon />
-            <address className="not-italic">01395 263509</address>
+            <PhoneIcon className="flex-shrink-0" />
+            <span className="not-italic">01395 263509</span>
           </div>
         </div>
       </div>
