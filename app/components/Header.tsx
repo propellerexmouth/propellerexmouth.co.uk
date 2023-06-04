ae4cf3d3580bfb7f1107f36f85ca888061f46670
Alex Howes <alex@pixelfridge.digital>
2023-06-04 16:11:15 +0100

    feat(Nav) Refactor to use the same nav for mobile and desktop, including Dynamic Logo (to hide wording on mobile), Size prop on social links and make nav link it's own component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,27 +1,41 @@
-import Link from "next/link";
-import React from "react";
-import LogoLong from "./LogoLong";
+'use client';
+
+import React, { useEffect, useState } from "react";
+import DynamicLogo from "./DynamicLogo";
 import NavLinks from "./NavLinks";
-import MobileNav from "./MobileNav";
+import { CloseIcon, MenuIcon } from "./Icons";
+import SocialLinks from "./SocialLinks";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.classList[isOpen ? 'add' : 'remove']("overflow-hidden");
+    document.body.classList.add("lg:overflow-auto");
+  }, [isOpen]);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <header className="hidden lg:flex w-full bg-white text-black">
-        <div id="desktop-header-container" className="mx-auto w-full max-w-6xl p-6">
-          <div className="flex justify-between items-center">
-            <h1 className="flex text-4xl items-center space-x-4">
-              <LogoLong />
+      <header className="flex w-full relative">
+        <div id="desktop-header-container" className="mx-auto w-full max-w-6xl p-6 ">
+          <div className="flex justify-between items-center gap-12">
+            <h1 className="flex text-4xl items-center z-[60]">
+              <DynamicLogo />
             </h1>
-            <nav className="flex items-center justify-between space-x-8">
-              <NavLinks incHome={false} incContact={false} />
-            </nav>
+            <button onClick={toggleMenu} className="block lg:hidden z-[60]">{isOpen ? <CloseIcon className="h-12 w-12 px-2 py-1 stroke-2 cursor-pointer font-bold" /> : <MenuIcon className="h-12 w-12 px-2 py-1 stroke-2 cursor-pointer font-bold" />}</button>
+            <div className={`fixed flex flex-col bg-white text-center z-50 top-0 left-0 right-0 h-full transition duration-300 -translate-x-full lg:static lg:translate-x-0  ${isOpen ? 'translate-x-0' : ''}`}>
+              <div className="h-[96px] flex-shrink-0 lg:hidden"></div>
+              <div className="overflow-auto p-12 lg:p-0">
+                <nav className="mb-8 lg:mb-0 flex flex-col lg:flex-row items-center justify-center gap-12 text-xl lg:text-base">
+                  <NavLinks incHome={false} incContact={false} showIcons={true} />
+                </nav>
+                <div className="lg:hidden mt-auto"><SocialLinks size="lg" /></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </header>
-      <header id="mobile-header-container" className="flex lg:hidden w-full bg-white text-black">
-        <div className="flex justify-between w-full items-center p-4">
-          <MobileNav />
         </div>
       </header>
     </>
