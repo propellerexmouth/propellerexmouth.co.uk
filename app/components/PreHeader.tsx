3b5f81e5e0fb067580c56433c847da15ac5a3f05
Charles Farrelly <charles@pixelfridge.digital>
2024-05-22 12:25:40 +0100

    feat(PreHeader) Join button added

--- app/components/PreHeader.tsx
+++ app/components/PreHeader.tsx
@@ -7,6 +7,10 @@ const PreHeader = () => {
   return (
     <nav className="hidden lg:flex w-full bg-grey-20 px-6 text-black">
       <div id="social-container" className="mx-auto max-w-6xl py-2 pr-6 flex w-full justify-end items-center space-x-12">
+        <Link className="hover:underline flex items-center space-x-1" href="/spaces">
+          <span>Join</span>
+          <UpRightArrow className="text-primary-900 h-4 w-4" />
+        </Link>
         <Link className="hover:underline flex items-center space-x-1" href="/contact">
           <span>Contact</span>
           <UpRightArrow className="text-primary-900 h-4 w-4" />
