cce5b58916caa4a07aeb6ba0feaade1c1b72940c
Charles Farrelly <charles@pixelfridge.digital>
2024-05-23 10:57:12 +0100

    feat(images) Replaced stock image and update spaces cards

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -76,7 +76,7 @@ const AboutPage = () => {
 
         <div className="flex items-center justify-between mt-8 flex-col md:flex-row">
           <div className="w-fit md:w-[18%]">
-            <Image src="/images/team/Jim.jpg" alt="placeholder image" className="object-cover h-full w-auto aspect-square" width="100" height="100"/>
+            <Image src="/images/team/jim.jpg" alt="placeholder image" className="object-cover h-full w-auto aspect-square" width="100" height="100"/>
           </div>
           <div className="w-full md:w-[78%] md:text-left text-center my-8 md:my-0">
             <h4 className="font-bold uppercase text-md">
