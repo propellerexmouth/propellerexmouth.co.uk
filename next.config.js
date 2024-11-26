c13b775b5c7a7ad2c3905440620f9519b3bef034
Jim Hill <ukjimbobbery@gmail.com>
2024-11-26 16:55:56 +0000

    Update next.config.js
    Added redirect for /join

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
     compiler: {
       styledComponents: true
+    },
+    async redirects() {
+        return [
+          {
+            source: '/join',
+            destination: '/',
+            permanent: true,
+          },
+        ]
     }
 }
 
